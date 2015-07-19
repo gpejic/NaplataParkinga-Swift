c061d3b602c8259ee7747fd442811c77b43f02f8
Goran Pejic <goran.pejic@virginpulse.com>
2015-07-19 11:32:12 -0400

    Created Xcode project

---
+++ NaplataParkinga/NaplataParkinga/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NaplataParkinga
+//
+//  Created by Goran Pejic on 19/07/15.
+//  Copyright (c) 2015 Goran Pejic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NaplataParkinga/NaplataParkingaTests/NaplataParkingaTests.swift
@@ -0,0 +1,36 @@
+//
+//  NaplataParkingaTests.swift
+//  NaplataParkingaTests
+//
+//  Created by Goran Pejic on 19/07/15.
+//  Copyright (c) 2015 Goran Pejic. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class NaplataParkingaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
